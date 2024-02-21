951844f878eaf890bddeda404dff794e9d424ebc
Marko Paunovic <marpau970514@hotmail.com>
2024-02-21 11:46:16 +0100

    Initial Commit

---
+++ Post-Pulse/Post_PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Post_PulseApp.swift
+//  Post-Pulse
+//
+//  Created by Marko Paunovic on 2024-02-21.
+//
+
+import SwiftUI
+
+@main
+struct Post_PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
