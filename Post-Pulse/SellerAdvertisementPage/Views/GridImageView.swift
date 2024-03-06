96ad373677e0c6a66ae61ce6f62634337fc319f9
Marko Paunovic <marpau970514@hotmail.com>
2024-03-06 11:37:16 +0100

    Images are now shown in a GridView and indicates the remaining images, going to implement a button to show all images

--- Post-Pulse/SellerAdvertisementPage/Views/GridImageView.swift
+++ Post-Pulse/SellerAdvertisementPage/Views/GridImageView.swift
@@ -11,46 +11,50 @@ struct GridImageView: View {
     
     @EnvironmentObject var theItem: ItemViewModel
     let numberOfImagesToShow = 4
-        
-        let index: Int
-        let imageName: String
-        let item: Item
-        
-        init(index: Int, imageName: String, item: Item) {
-            self.index = index
-            self.imageName = imageName
-            self.item = item
-        }
+
+    let item: Item
+    let index: Int
+    
+    init(index: Int, item: Item) {
+
+        self.item = item
+        self.index = index
+    }
     
     var body: some View {
         ZStack {
-            ForEach(item.image, id: \.self) { image in
-                Image(image)
+            if index <= 3 {
+                let imageName = item.image[index]
+                Image(imageName)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .frame(width: (getRect().width - 20) / 2, height: 120)
             }
-        }
+            
+            let imageName = item.image
             if imageName.count > 4 && index == 3 {
                 RoundedRectangle(cornerRadius: 5)
                     .fill(Color.black.opacity(0.5))
                 
-                let remainingImages = index
+                let remainingImages = imageName.count - 4
                 Text("+\(remainingImages)")
                     .font(.title)
-                    .fontWeight(.heavy)
+                    .fontWeight(.bold)
                     .foregroundColor(.white)
-                
             }
         }
     }
-
-
-struct GridImageView_Previews: PreviewProvider {
-    static var previews: some View {
-        GridImageView(index: 1, imageName: "passat rear", item: Item.init(name: "passat 2016", image: ["passat interior", "passat sido"], description: "fin", price: "10", category: .fordon))
-    }
 }
+    
+    
+    
+    struct GridImageView_Previews: PreviewProvider {
+        static var previews: some View {
+            SellerAdvertisementView(item: Item(name: "Passat 2016", image: ["passat sido", "passat rear", "passat interior", "passat profile", "passat seats", "passat rims"], description: "*KXG882*, ABS-bromsar, ACC/2-zons Klimatanläggning, Adaptiv farthållare, Airbag förare, Airbag passagerare fram, Airbag passagerare urkopplingsbar, Android Auto, Antisladd, Antispinn, Apple carplay, AUX-ingång, AWD, Backkamera, Bluetooth, CD/Radio, Dieselvärmare fjärrstyrd, Dragkrok utfällbar, Elbaklucka, Elhissar fram  Skinnklädsel, Sommardäck på 18 aluminiumfälgar, Start-/stoppfunktion, Svensksåld, Sätesvärme fram, Tonade rutor, USB-ingång", price: "999000", category: .fordon))
+                .environmentObject(ItemViewModel())
+        }
+    }
+
 
 extension View {
   func getRect() -> CGRect {
